21ae43c8cc077de48ea60b1310f48f384c7929b3
Samgrass <samuelegrassetti01@gmail.com>
2024-10-14 17:45:08 +0200

    create apartment 'mmscr'

---
+++ database/migrations/2024_10_14_140828_create_apartments_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('title');
+            $table->string('slug');
+            $table->unsignedInteger('room');
+            $table->unsignedInteger('beds');
+            $table->unsignedInteger('bathroom');
+            $table->smallInteger('mq');
+            $table->string('address');
+            $table->decimal('longitude', 9, 6);
+            $table->decimal('latitude', 9, 6);
+            $table->string('img');
+            $table->boolean('is_visible')->default(true);
+            $table->timestamps();
+
+            // foreign user
+            $table->foreign('user_id')->references('id')->on('users')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('apartments', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropIfExists('apartments');
+        });
+    }
+};
---
+++ database/seeders/ApartmentSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Functions\Helper;
+use App\Models\Apartment;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ApartmentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $apartments = config('apartment');
+        foreach ($apartments as $apartment) {
+            $new_apartment = new Apartment();
+            $new_apartment->user_id = $apartment['user_id'];
+            $new_apartment->title = $apartment['title'];
+            $new_apartment->slug = Helper::generateSlug($new_apartment->title, Apartment::class);
+            $new_apartment->room = $apartment['room'];
+            $new_apartment->beds = $apartment['beds'];
+            $new_apartment->bathroom = $apartment['bathroom'];
+            $new_apartment->mq = $apartment['mq'];
+            $new_apartment->address = $apartment['address'];
+            $new_apartment->longitude = $apartment['longitude'];
+            $new_apartment->latitude = $apartment['latitude'];
+            $new_apartment->img = $apartment['img'];
+            $new_apartment->is_visible = $apartment['is_visible'];
+
+            // Salva il nuovo appartamento nel database
+            $new_apartment->save();
+        }
+    }
+}
