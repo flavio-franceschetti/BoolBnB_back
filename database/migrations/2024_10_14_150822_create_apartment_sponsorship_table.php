d1ad102d8d8687c60b01400f6367a35e7f337fbc
Samgrass <samuelegrassetti01@gmail.com>
2024-10-14 17:49:01 +0200

    create table many to many for apartment_sponsorship

---
+++ database/migrations/2024_10_14_150822_create_apartment_sponsorship_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('apartment_sponsorship', function (Blueprint $table) {
+            $table->id();
+            // apartment
+            $table->unsignedBigInteger('apartment_id');
+            $table->foreign('apartment_id')->references('id')->on('apartments')->cascadeOnDelete();
+
+            // sponsorship
+            $table->unsignedBigInteger('sponsorship_id');
+            $table->foreign('sponsorship_id')->references('id')->on('sponsorships')->cascadeOnDelete();
+
+            $table->timestamp('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('apartment_sponsorship');
+    }
+};
