79555ee9ff1953d74908c346b5eaa1524a8bdfcb
Simone Menichetti <simon3menichetti95@gmail.com>
2024-10-15 16:47:23 +0200

    feat:(add sponsorship model and confic)

--- app/Models/Sponsorship.php
+++ app/Models/Sponsorship.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Sponsorship extends Model
 {
     use HasFactory;
+
+    // definizione delle fillable per sponsorship
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'price',
+        'duration',
+    ];
+
+
+    // relazione con apartment
+
+    public function apartment()
+    {
+        // una sponsorizzazione appartiene ad un appartamento
+        return $this->belongsToMany(Apartment::class, 'apartment_sponsorship')
+            // gestione dei timestamp
+            ->withTimestamps()
+            // inclusione di end_date nella relazione
+            ->withPivot('end_date');
+    }
 }
