d08c9e0f3a5171ce3858862ed363d1ea18ae748a
Simone Menichetti <simon3menichetti95@gmail.com>
2024-10-15 16:46:25 +0200

    feat:(add Models aparrtment)

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -8,4 +8,50 @@ use Illuminate\Database\Eloquent\Model;
 class Apartment extends Model
 {
     use HasFactory;
+
+    // definizione delle fillable
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'slug',
+        'room',
+        'beds',
+        'bathroom',
+        'mq',
+        'address',
+        'longitude',
+        'latitude',
+        'img',
+        'is_visible',
+    ];
+
+    // relazione con sponsorships
+    public function sponsorships()
+    {
+        return $this->hasMany(Sponsorship::class);
+    }
+
+    // relazione con messages
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+    // relazione con views
+
+    public function views()
+
+    {
+        return $this->hasMany(View::class);
+    }
+
+    // relazione con  services
+
+    public function Services()
+    {
+        // collegamento con la tabella pivot apartment_service
+        return $this->belongsToMany(Service::class, 'apartment_service')
+            ->withTimestamps();
+    }
 }
