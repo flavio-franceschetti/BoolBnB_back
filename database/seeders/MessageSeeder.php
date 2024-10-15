e04b7fa369fe8d815e477f017af8ae8eac3c313b
Simone Menichetti <simon3menichetti95@gmail.com>
2024-10-15 16:50:33 +0200

    feat.(add message seeder)

--- database/seeders/MessageSeeder.php
+++ database/seeders/MessageSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Message;
+use Faker\Factory as Faker;
 
 class MessageSeeder extends Seeder
 {
@@ -12,6 +15,26 @@ class MessageSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = Faker::create();
+
+        // Ottieni una lista di apartment_id esistenti
+        $apartmentIds = DB::table('apartments')->pluck('id')->toArray();
+
+        // recupero i messaggi random dal file config
+
+        $messages = config('messages.messages');
+
+        for ($i = 0; $i < 30; $i++) {
+            $new_message = new Message();
+            $new_message->apartment_id = $faker->randomElement($apartmentIds);
+            $new_message->name = $faker->firstName;
+            $new_message->surname = $faker->lastName;
+            $new_message->email = $faker->unique()->safeEmail;
+            // prende randomicamente un messaggio dal file data config messages.php
+            $new_message->content = $faker->randomElement($messages);
+
+            // Salva il nuovo messaggio nel database
+            $new_message->save();
+        }
     }
 }
