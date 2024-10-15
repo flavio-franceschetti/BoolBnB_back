a4923a43055cce36f1be24d241b1f86d1db41122
Samgrass <samuelegrassetti01@gmail.com>
2024-10-15 12:13:39 +0200

    fix user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50);
-            $table->string('surname', 50);
+            $table->string('name', 50)->nullable();
+            $table->string('surname', 50)->nullable();
             $table->date('date_of_birth')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
