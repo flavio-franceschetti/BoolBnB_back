ab5be7c9db00e4a85286d0ef84b8248ab3d43ddf
Simone Menichetti <simon3menichetti95@gmail.com>
2024-10-15 16:48:24 +0200

    feat:(add file config data)

---
+++ config/apartment_services.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    ['name' => 'Wi-Fi'],
+    ['name' => 'Piscina'],
+    ['name' => 'Parcheggio'],
+    ['name' => 'Aria Condizionata'],
+    ['name' => 'TV Satellitare'],
+    ['name' => 'Lavatrice'],
+    ['name' => 'Palestra'],
+    ['name' => 'Piscina'],
+    ['name' => 'Giardino'],
+    ['name' => 'Idromassaggio'],
+    ['name' => 'Cucina'],
+];
