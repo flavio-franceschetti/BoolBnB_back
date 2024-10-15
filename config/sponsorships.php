ced254afd1cbcbb096a54f4be1df84e5a28dcdc4
Simone Menichetti <simon3menichetti95@gmail.com>
2024-10-15 16:49:45 +0200

    feat:(add config sponsorships & data)

---
+++ config/sponsorships.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'sponsorships' => [
+        ['name' => 'Bronze Package', 'price' => 2.99, 'duration' => 24],
+        ['name' => 'Silver Package', 'price' => 5.99, 'duration' => 72],
+        ['name' => 'Gold Package', 'price' => 9.99, 'duration' => 144],
+    ],
+];
