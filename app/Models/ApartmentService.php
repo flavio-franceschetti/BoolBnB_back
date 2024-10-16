ead41abaabcc05cd339469ccffd1647e9bf09285
Simone Menichetti <simon3menichetti95@gmail.com>
2024-10-16 10:56:25 +0200

    feat:( add Apartment model and define table & relation)

---
+++ app/Models/ApartmentService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ApartmentService extends Model
+{
+    use HasFactory;
+
+    // definisco il nome della tabella
+
+    protected $table = 'apartment_service';
+
+    // definisco le fillable
+
+    protected $fillable = [
+        'apartment_id',
+        'service_id',
+
+    ];
+
+    // relaziono con apartment
+
+    public function apartment()
+    {
+        return $this->belongsTo(Apartment::class);
+    }
+
+    // relaziono con service
+
+    public function service()
+    {
+        return $this->belongsTo(Service::class);
+    }
+}
