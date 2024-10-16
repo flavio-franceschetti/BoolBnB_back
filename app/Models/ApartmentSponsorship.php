2b433c53f2f7e6465022dd7af53abf76f3b5b503
Simone Menichetti <simon3menichetti95@gmail.com>
2024-10-16 10:57:02 +0200

    feat:(add ApartmentSponsorship Pivot model & define relation )

---
+++ app/Models/ApartmentSponsorship.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ApartmentSponsorship extends Model
+{
+    use HasFactory;
+
+    // definizione nome table
+    protected $table = 'apartment_sponsorship';
+
+    // definizione delle fillable
+
+    protected $fillable = [
+        'apartment_id',
+        'sponsorship_id',
+        'end_date',
+    ];
+
+    // relazione con apartment
+
+    public function apartment()
+    {
+        return $this->belongsTo(Apartment::class);
+    }
+
+    // relazione con  Sponsorship
+
+    public function sponsorship()
+
+    {
+        return $this->belongsTo(Sponsorship::class);
+    }
+}
