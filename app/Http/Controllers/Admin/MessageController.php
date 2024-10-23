149b85d48ea430118e8a1fafd431715aefdf964f
EdoardoRaimondoSenis <166326043+EdoardoRaimondoSenis@users.noreply.github.com>
2024-10-23 11:13:55 +0200

    feat: add store Message

---
+++ app/Http/Controllers/Admin/MessageController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Message;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class MessageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $messages = Message::orderBy('id')->where('apartment_id', Auth::id())->get();
+
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // Validazione dei dati inviati dall'utente
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'surname' => 'required|max:255',
+            'email' => 'required|email',
+            'content' => 'required|min:10',
+        ]);
+
+        // Creazione di un nuovo contatto nel database
+        $message = Message::create($validatedData);
+
+        // Risposta JSON di successo con i dati inseriti
+        return response()->json([
+            'message' => 'Contatto creato con successo!',
+            'data' => $message
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
