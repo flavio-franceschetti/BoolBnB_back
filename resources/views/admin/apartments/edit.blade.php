b98e0d73b712d2f74fef4244c1078852942ddf9c
Simone Menichetti <simon3menichetti95@gmail.com>
2024-10-24 18:01:29 +0200

    fix:(add validation edit)

--- resources/views/admin/apartments/edit.blade.php
+++ resources/views/admin/apartments/edit.blade.php
@@ -301,8 +301,10 @@
                 // Controllo dimensione file
                 let valid = true;
                 for (let i = 0; i < files.length; i++) {
-                    if (files[i].size > 10 * 1024 * 1024) {
-                        showError('images', 'Ogni immagine deve essere al massimo di 10 MB.');
+                    if (files[i].size > 2 * 1024 * 1024) { // Cambiato a 2 MB
+                        showError('images',
+                            'Il file che stai cercando di caricare è superiore a 2 MB e non è accettato.'
+                        );
                         valid = false;
                         break;
                     }
