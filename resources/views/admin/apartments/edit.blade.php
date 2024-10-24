8ce7d2272110cf645c8f1830ad3fa0bc8d0f8a73
Simone Menichetti <simon3menichetti95@gmail.com>
2024-10-24 02:50:20 +0200

    fix:(layout edit )

--- resources/views/admin/apartments/edit.blade.php
+++ resources/views/admin/apartments/edit.blade.php
@@ -27,26 +27,26 @@
         </div>
 
         <!-- Proprietà dell'appartamento -->
-        <div class="d-flex gap-3 mb-3">
-            <div>
+        <div class="row mb-3">
+            <div class="col-md-3">
                 <label for="rooms" class="form-label">N. Camere</label>
                 <input type="number" class="form-control" id="rooms" name="rooms"
                     value="{{ old('rooms', $apartment->rooms) }}" required min="1">
                 <small class="text-danger" id="roomsError" style="display: none;"></small>
             </div>
-            <div>
+            <div class="col-md-3">
                 <label for="beds" class="form-label">N. Letti</label>
                 <input type="number" class="form-control" id="beds" name="beds"
                     value="{{ old('beds', $apartment->beds) }}" required min="1">
                 <small class="text-danger" id="bedsError" style="display: none;"></small>
             </div>
-            <div>
+            <div class="col-md-3">
                 <label for="bathrooms" class="form-label">N. Bagni</label>
                 <input type="number" class="form-control" id="bathrooms" name="bathrooms"
                     value="{{ old('bathrooms', $apartment->bathrooms) }}" required min="1">
                 <small class="text-danger" id="bathroomsError" style="display: none;"></small>
             </div>
-            <div>
+            <div class="col-md-3">
                 <label for="mq" class="form-label">Metri Quadri</label>
                 <input type="number" class="form-control" id="mq" name="mq"
                     value="{{ old('mq', $apartment->mq) }}" required min="1">
@@ -58,13 +58,19 @@
         <div id="search-box-container" class="mb-3"></div>
 
         <!-- Servizi -->
-        <fieldset class="btn-group mb-3" role="group" aria-label="Servizi">
+        <fieldset class="mb-3">
             <legend>Servizi</legend>
-            @foreach ($services as $service)
-                <input name="services[]" type="checkbox" value="{{ $service->id }}" class="btn-check"
-                    id="service-{{ $service->id }}" autocomplete="off" @if (in_array($service->id, old('services', $apartment->services->pluck('id')->toArray()))) checked @endif>
-                <label class="btn btn-outline-primary" for="service-{{ $service->id }}">{{ $service->name }}</label>
-            @endforeach
+            <div class="d-flex flex-wrap gap-2">
+                @foreach ($services as $service)
+                    <div>
+                        <input name="services[]" type="checkbox" value="{{ $service->id }}" class="btn-check"
+                            id="service-{{ $service->id }}" autocomplete="off"
+                            @if (in_array($service->id, old('services', $apartment->services->pluck('id')->toArray()))) checked @endif>
+                        <label class="btn btn-outline-primary"
+                            for="service-{{ $service->id }}">{{ $service->name }}</label>
+                    </div>
+                @endforeach
+            </div>
             <small class="text-danger" id="servicesError" style="display: none;"></small>
         </fieldset>
 
@@ -73,15 +79,14 @@
             <label>Immagini attuali dell'appartamento:</label>
             <div class="row">
                 @foreach ($apartment->images as $image)
-                    <div class="col-md-2 mb-3">
+                    <div class="col-md-2 mb-3 text-center">
                         <img src="{{ asset('storage/' . $image->img_path) }}" alt="{{ $image->img_name }}"
                             class="img-fluid">
                         <div class="form-check mt-2">
                             <input type="checkbox" name="delete_images[]" value="{{ $image->id }}"
                                 class="form-check-input delete-checkbox" id="delete-image-{{ $image->id }}"
                                 autocomplete="off">
-                            <label class="form-check-label" for="delete-image-{{ $image->id }}">Elimina questa
-                                immagine</label>
+                            <label class="form-check-label" for="delete-image-{{ $image->id }}">Elimina</label>
                         </div>
                         <input type="hidden" name="existing_images[]" value="{{ $image->id }}">
                     </div>
@@ -91,7 +96,7 @@
             <small class="text-info" id="imagesCount" style="display: none;"></small>
         </div>
 
-        <!-- Immagine -->
+        <!-- Aggiungi immagini -->
         <div class="mb-3">
             <label for="images" class="form-label">Modifica o aggiungi Immagini (opzionale)</label>
             <input class="form-control" type="file" id="images" name="images[]" multiple accept="image/*">
@@ -99,24 +104,18 @@
         </div>
 
         <!-- Visibilità -->
-        <fieldset class="is-visible-radios mb-3">
+        <fieldset class="mb-3">
             <legend>Visibilità</legend>
             <div class="form-check">
                 <input class="form-check-input" type="radio" name="is_visible" value="1" id="flexRadioDefault1"
                     checked>
-                <label class="form-check-label" for="flexRadioDefault1"> Si </label>
+                <label class="form-check-label" for="flexRadioDefault1">Si</label>
             </div>
             <div class="form-check">
                 <input class="form-check-input" type="radio" name="is_visible" value="0" id="flexRadioDefault2">
-                <label class="form-check-label" for="flexRadioDefault2"> No </label>
+                <label class="form-check-label" for="flexRadioDefault2">No</label>
             </div>
         </fieldset>
-
-        <!-- Pulsanti -->
-        <div class="mb-3">
-            <input type="submit" class="btn btn-primary" value="Modifica">
-            <input type="reset" class="btn btn-danger" value="Annulla">
-        </div>
 
         <!-- Sponsorship Selection -->
         <div class="form-group mb-3">
@@ -127,9 +126,15 @@
                     </option>
                 @endforeach
             </select>
-            <div class="mb-3">
+            <div class="mt-3">
                 <a href="#" class="btn btn-success" id="paymentLink">Sponsorizza Appartamento</a>
             </div>
+        </div>
+
+        <!-- Pulsanti -->
+        <div class="d-flex justify-content-between mt-4">
+            <input type="submit" class="btn btn-primary" value="Modifica">
+            <input type="reset" class="btn btn-danger" value="Annulla">
         </div>
     </form>
 
@@ -213,7 +218,7 @@
                     paymentLink.style.display = 'inline'; // Mostra il link se non è visibile
                 } else {
                     paymentLink.style.display =
-                    'none'; // Nascondi se nessuna sponsorizzazione è selezionata
+                        'none'; // Nascondi se nessuna sponsorizzazione è selezionata
                 }
             });
         });
