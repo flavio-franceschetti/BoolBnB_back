a6b7a8b7ca4ac47acefee392a630d3038b958c76
Simone Menichetti <simon3menichetti95@gmail.com>
2024-10-28 13:13:12 +0100

    feat.(add redirect to page into aside)

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -2,8 +2,10 @@
     <ul class="aside-nav d-flex flex-column gap-2">
         <li><a href="{{ route('admin.home') }}"><i class="fa-solid fa-house"></i> Home</a></li>
         <li><a href="{{ route('admin.apartments.index') }}"><i class="fa-solid fa-house"></i> Appartamenti</a></li>
-        <li><a href="{{ route('admin.apartments.create') }}"><i class="fa-solid fa-house"></i> Inserisci appartamento</a></li>
+        <li><a href="{{ route('admin.apartments.create') }}"><i class="fa-solid fa-house"></i> Inserisci appartamento</a>
+        </li>
         <li><a href="{{ route('admin.messages.index') }}"><i class="fa-solid fa-house"></i> Messaggi</a></li>
-       
+        <li><a href="{{ route('admin.sponsorships.index') }}"><i class="fa-solid fa-star"></i> Sponsorizzazioni</a></li>
+
     </ul>
 </aside>
