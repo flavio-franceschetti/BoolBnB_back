d9d949ecd78998cc75fb40ce52c9cf94bd2c6bf4
flavio-franceschetti <flaviofranceschetti98@gmail.com>
2024-10-31 16:12:37 +0100

    fix: messages layout

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -1,39 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
-@if (count($messages) > 0)
-<h1>Messaggi ricevuti</h1>
-<div class="container-fluid">
-    <div class="row">
-        @foreach ($messages as $message)
-        <div class="card m-3 col-6" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">{{ $message->apartment->title }}</h5>
-                <p class="card-title">{{ $message->apartment->address }}</p>
-                <h5 class="card-title">Da: {{ $message->name }} {{ $message->surname }}</h5>
-                <h6 class="card-subtitle mb-2 text-body-secondary">Email: {{ $message->email }}</h6>
-                <div>{{ $message->created_at->format('d-m-Y H:i') }}</div>
-                <p class="card-text">{{ $message->content }}</p>
-                <div class="d-flex gap-3">
-                    <a href="{{ route('admin.messages.show', $message) }}" class="btn btn-primary">Visualizza</a>
-                </div>
-            </div>
-        </div>
-        @endforeach
+    <div class="p-4">
+        @if (count($messages) > 0)
+            <h1>{{ count($messages) }} messaggi ricevuti</h1>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Mittente</th>
+                        <th scope="col">Appartamento</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Ricevuto il</th>
+                        <th scope="col">Info</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($messages as $message)
+                        <tr>
+                            <td>{{ $message->name }}</td>
+                            <td>{{ $message->surname }}</td>
+                            <td>{{ $message->email }}</td>
+                            <td>{{ $message->created_at->format('d-m-Y | H:i') }}</td>
+                            <td><a href="{{ route('admin.messages.show', $message) }}" class="btn btn-primary">Visualizza</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @else
+            <h3>Non hai ricevuto nessun messaggio per il momento</h3>
+        @endif
     </div>
-</div>
-@else
-<h3>Non hai ricevuto nessun messaggio per il momento</h3>
-@endif
-<style>
-    .card-text {
-        white-space: nowrap;
-        /* Impedisce che il testo vada a capo */
-        overflow: hidden;
-        /* Nasconde il testo in eccesso */
-        text-overflow: ellipsis;
-        /* Aggiunge i puntini di sospensione (...) */
-        width: 200px;
-    }
-</style>
+
+    <style>
+        .card-text {
+            white-space: nowrap;
+            /* Impedisce che il testo vada a capo */
+            overflow: hidden;
+            /* Nasconde il testo in eccesso */
+            text-overflow: ellipsis;
+            /* Aggiunge i puntini di sospensione (...) */
+            width: 200px;
+        }
+    </style>
 @endsection
