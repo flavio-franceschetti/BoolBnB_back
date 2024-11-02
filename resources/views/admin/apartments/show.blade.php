9b21585feaf568749120212c989f35ddbddee18f
Samgrass <samuelegrassetti01@gmail.com>
2024-11-02 16:39:50 +0100

    fix Apartment show date localization

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -1,153 +1,154 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 offset-md-2">
-                <div class="card mb-4">
-                    <div class="card-header bg-primary text-white d-flex justify-content-between align-items-center">
-                        <h2>Dettagli appartamento</h2>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 offset-md-2">
+            <div class="card mb-4">
+                <div class="card-header bg-primary text-white d-flex justify-content-between align-items-center">
+                    <h2>Dettagli appartamento</h2>
+                </div>
+
+                @if (session('success'))
+                <div class="alert alert-success">
+                    {{ session('success') }}
+                </div>
+                @endif
+
+                {{-- IMMAGINE APPARTAMENTO --}}
+                @if ($apartment->images)
+                <div class="card">
+                    <div class="card-header bg-secondary text-white">
+                        <h4>Immagini dell'appartamento</h4>
                     </div>
-
-                    @if (session('success'))
-                        <div class="alert alert-success">
-                            {{ session('success') }}
-                        </div>
-                    @endif
-
-                    {{-- IMMAGINE APPARTAMENTO --}}
-                    @if ($apartment->images)
-                        <div class="card">
-                            <div class="card-header bg-secondary text-white">
-                                <h4>Immagini dell'appartamento</h4>
-                            </div>
-                            <div class="card-body">
-                                <div class="row">
-                                    @foreach ($apartment->images as $image)
-                                        <div class="col-md-4 mb-3">
-                                            <img src="{{ asset('storage/' . $image->img_path) }}" class="img-fluid rounded"
-                                                alt="Immagine appartamento">
-                                        </div>
-                                    @endforeach
-                                </div>
-                            </div>
-                        </div>
-                    @else
-                        <div class="card">
-                            <div class="card-body text-center">
-                                <img src="/img/no-image.jpg" class="img-fluid" alt="Immagine non disponibile">
-                            </div>
-                        </div>
-                    @endif
-
-                    {{-- INIZIO CARD PER I DETTAGLI --}}
                     <div class="card-body">
-                        <div class="row mb-3">
-                            <div class="col-md-6">
-                                <p><strong>
-                                        <i class="fas fa-door-open"></i>
-                                        Stanze:
-                                    </strong> {{ $apartment->rooms }}</p>
-                                <p><strong>
-                                        <li class="fas fa-bed"></li>
-                                        Letti:
-                                    </strong> {{ $apartment->beds }}</p>
-                                <p><strong>
-                                        <li class="fas fa-toilet"></li>
-                                        Bagni:
-                                    </strong>{{ $apartment->bathrooms }}</p>
-                                <p><strong>
-                                        Metri quadrati: </strong>{{ $apartment->mq }} <strong>m²</strong></p>
+                        <div class="row">
+                            @foreach ($apartment->images as $image)
+                            <div class="col-md-4 mb-3">
+                                <img src="{{ asset('storage/' . $image->img_path) }}" class="img-fluid rounded"
+                                    alt="Immagine appartamento">
                             </div>
-                            <div class="col-md-6">
-                                <i class="fa-solid fa-street-view"></i><strong>
-                                    Indirizzo: </strong> {{ $apartment->address }} {{ $apartment->civic_number }}</p>
-                                <p><strong>Visibile:</strong>
-                                    @if ($apartment->is_visible)
-                                        <i class="fa-solid fa-eye"></i> Si
-                                    @else
-                                        <i class="fa-solid fa-eye-slash"></i> No
-                                    @endif
-                                </p>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+                @else
+                <div class="card">
+                    <div class="card-body text-center">
+                        <img src="/img/no-image.jpg" class="img-fluid" alt="Immagine non disponibile">
+                    </div>
+                </div>
+                @endif
 
-                                {{-- Sezione Servizi --}}
-                                <div class="mb-3">
-                                    <h4>Servizi dell'appartamento</h4>
-                                    @if ($apartment->services)
-                                        @foreach ($apartment->services as $service)
-                                            <span class="badge bg-warning">{{ $service->name }}</span>
-                                        @endforeach
-                                    @else
-                                        <p>Nessun servizio disponibile</p>
-                                    @endif
-                                </div>
+                {{-- INIZIO CARD PER I DETTAGLI --}}
+                <div class="card-body">
+                    <div class="row mb-3">
+                        <div class="col-md-6">
+                            <p><strong>
+                                    <i class="fas fa-door-open"></i>
+                                    Stanze:
+                                </strong> {{ $apartment->rooms }}</p>
+                            <p><strong>
+                                    <li class="fas fa-bed"></li>
+                                    Letti:
+                                </strong> {{ $apartment->beds }}</p>
+                            <p><strong>
+                                    <li class="fas fa-toilet"></li>
+                                    Bagni:
+                                </strong>{{ $apartment->bathrooms }}</p>
+                            <p><strong>
+                                    Metri quadrati: </strong>{{ $apartment->mq }} <strong>m²</strong></p>
+                        </div>
+                        <div class="col-md-6">
+                            <i class="fa-solid fa-street-view"></i><strong>
+                                Indirizzo: </strong> {{ $apartment->address }} {{ $apartment->civic_number }}</p>
+                            <p><strong>Visibile:</strong>
+                                @if ($apartment->is_visible)
+                                <i class="fa-solid fa-eye"></i> Si
+                                @else
+                                <i class="fa-solid fa-eye-slash"></i> No
+                                @endif
+                            </p>
 
-                                <div class="text-muted">
-                                    <p><strong>Data Pubblicazione:</strong> {{ $apartment->created_at->format('d F Y') }}
-                                    </p>
-                                </div>
-                                @if ($primarySponsorshipName)
-                                    <div class="card mb-4">
-                                        <div class="card-header bg-primary text-white">
-                                            <h5 class="mb-0">Sponsorizzazione Attiva</h5>
-                                        </div>
-                                        <div class="card-body">
-                                            <h6 class="card-title">Opzione Pacchetto: {{ $primarySponsorshipName }}</h6>
-                                            <p class="card-text">
-                                                <strong>Tempo Rimanente:</strong>
-                                                {{ $remainingHours }} ore e {{ $remainingMinutes }} minuti
-                                            </p>
-                                            <hr>
-                                            <p class="text-muted">
-                                                Questa sponsorizzazione ti offre una maggiore visibilità e opportunità per
-                                                il tuo appartamento!
-                                            </p>
-                                        </div>
-                                    </div>
+                            {{-- Sezione Servizi --}}
+                            <div class="mb-3">
+                                <h4>Servizi dell'appartamento</h4>
+                                @if ($apartment->services)
+                                @foreach ($apartment->services as $service)
+                                <span class="badge bg-warning">{{ $service->name }}</span>
+                                @endforeach
+                                @else
+                                <p>Nessun servizio disponibile</p>
                                 @endif
                             </div>
-                        </div>
 
-                        {{-- prova mappa --}}
-                        <div class="map-container">
-                            <div style="width: 700px; height: 500px" id="map"></div>
-                        </div>
-
-                        {{-- STATISTICHE DELLE VISUALIZZAZIONI --}}
-                        <h1>Statistiche per {{ $apartment->title }}</h1>
-                        <p>Totale Visualizzazioni: {{ $totalViews }}</p>
-                        <p>Visualizzazioni Oggi: {{ $dailyViews }}</p>
-
-                        {{-- Card per il grafico --}}
-                        <div class="card mb-4">
-                            <div class="card-header bg-secondary text-white d-flex justify-content-between">
-                                <h4 id="chartTitle">Visualizzazioni negli ultimi 12 mesi</h4>
-
-                                <select id="timeframe" class="form-select" style="width: auto;">
-                                    <option value="daily">Giornaliero</option>
-                                    <option value="monthly" selected>Mensile</option>
-                                    <option value="yearly">Annuale</option>
-                                </select>
+                            <div class="text-muted">
+                                <p><strong>Data Pubblicazione:</strong> {{ $apartment->created_at->translatedFormat('d F
+                                    Y') }}
+                                </p>
                             </div>
-                            <div class="card-body">
-                                <canvas id="viewsChart" width="400" height="200"></canvas>
+                            @if ($primarySponsorshipName)
+                            <div class="card mb-4">
+                                <div class="card-header bg-primary text-white">
+                                    <h5 class="mb-0">Sponsorizzazione Attiva</h5>
+                                </div>
+                                <div class="card-body">
+                                    <h6 class="card-title">Opzione Pacchetto: {{ $primarySponsorshipName }}</h6>
+                                    <p class="card-text">
+                                        <strong>Tempo Rimanente:</strong>
+                                        {{ $remainingHours }} ore e {{ $remainingMinutes }} minuti
+                                    </p>
+                                    <hr>
+                                    <p class="text-muted">
+                                        Questa sponsorizzazione ti offre una maggiore visibilità e opportunità per
+                                        il tuo appartamento!
+                                    </p>
+                                </div>
                             </div>
+                            @endif
                         </div>
+                    </div>
 
-                        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-                        {{-- BTN PER TORNARE ALL' ELENCO APPARTAMENTI --}}
-                        <div class="mt-4">
-                            <a href="{{ route('admin.apartments.index') }}" class="btn btn-primary">Torna all'elenco</a>
+                    {{-- prova mappa --}}
+                    <div class="map-container">
+                        <div style="width: 700px; height: 500px" id="map"></div>
+                    </div>
+
+                    {{-- STATISTICHE DELLE VISUALIZZAZIONI --}}
+                    <h1>Statistiche per {{ $apartment->title }}</h1>
+                    <p>Totale Visualizzazioni: {{ $totalViews }}</p>
+                    <p>Visualizzazioni Oggi: {{ $dailyViews }}</p>
+
+                    {{-- Card per il grafico --}}
+                    <div class="card mb-4">
+                        <div class="card-header bg-secondary text-white d-flex justify-content-between">
+                            <h4 id="chartTitle">Visualizzazioni negli ultimi 12 mesi</h4>
+
+                            <select id="timeframe" class="form-select" style="width: auto;">
+                                <option value="daily">Giornaliero</option>
+                                <option value="monthly" selected>Mensile</option>
+                                <option value="yearly">Annuale</option>
+                            </select>
                         </div>
+                        <div class="card-body">
+                            <canvas id="viewsChart" width="400" height="200"></canvas>
+                        </div>
+                    </div>
+
+                    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+                    {{-- BTN PER TORNARE ALL' ELENCO APPARTAMENTI --}}
+                    <div class="mt-4">
+                        <a href="{{ route('admin.apartments.index') }}" class="btn btn-primary">Torna all'elenco</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
-    {{-- script per far funzionare la mappa di tom tom --}}
-    <script>
-        // creo una constante dove inserisco la apiKey che prendo dal file config
+{{-- script per far funzionare la mappa di tom tom --}}
+<script>
+    // creo una constante dove inserisco la apiKey che prendo dal file config
         const apiKey = "{{ config('app.tomtomapikey') }}";
         const latitude = "{{ $apartment->latitude }}";
         const longitude = "{{ $apartment->longitude }}";
@@ -239,30 +240,30 @@
             viewsChart.data.datasets[0].data = newData;
             viewsChart.update();
         });
-    </script>
+</script>
 
-    <style>
-        .card {
-            border: 1px solid #007bff;
-            /* Colore del bordo */
-            border-radius: 0.5rem;
-            /* Raggio degli angoli */
-        }
+<style>
+    .card {
+        border: 1px solid #007bff;
+        /* Colore del bordo */
+        border-radius: 0.5rem;
+        /* Raggio degli angoli */
+    }
 
-        .card-header {
-            font-size: 1.25rem;
-            /* Dimensione del font */
-        }
+    .card-header {
+        font-size: 1.25rem;
+        /* Dimensione del font */
+    }
 
-        .card-title {
-            font-weight: bold;
-            /* Grassetto per il titolo */
-        }
+    .card-title {
+        font-weight: bold;
+        /* Grassetto per il titolo */
+    }
 
-        .card-text {
-            font-size: 1.1rem;
-            /* Dimensione del testo */
-        }
-    </style>
+    .card-text {
+        font-size: 1.1rem;
+        /* Dimensione del testo */
+    }
+</style>
 
 @endsection
