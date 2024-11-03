a6ecb555c3d1afa3d55ae61fe37964b7afc2fe06
Samgrass <samuelegrassetti01@gmail.com>
2024-11-03 23:34:59 +0100

    add message text truncate

--- resources/views/admin/messages/show.blade.php
+++ resources/views/admin/messages/show.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 <div class="p-4">
     <h3>Dettaglio del messaggio</h3>
-    <div>Titolo annuncio: {{ $message->apartment->title }}</div>
-    <div>Mittente: {{ $message->name }} {{ $message->surname }}</div>
-    <div>Email: {{ $message->email }}</div>
-    <div>Ricevuto il: {{ $message->created_at->format('d-m-Y') }}</div>
-    <div class="mb-3">Messaggio: {{ $message->content }}</div>
+    <div><strong>Titolo annuncio:</strong> {{ $message->apartment->title }}</div>
+    <div><strong>Mittente:</strong> {{ $message->name }} {{ $message->surname }}</div>
+    <div><strong>Email:</strong> {{ $message->email }}</div>
+    <div><strong>Ricevuto il:</strong> {{ $message->created_at->format('d-m-Y') }}</div>
+    <div class="mb-3"><strong>Messaggio:</strong> {{ $message->content }}</div>
     <a class="btn btn-primary" href="{{ route('admin.messages.index') }}">Torna ai messaggi</a>
 </div>
 @endsection
