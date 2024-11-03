02b02c396a26332bea20282aeb5d9946ca2c8449
Samgrass <samuelegrassetti01@gmail.com>
2024-11-03 18:42:03 +0100

    fix date of birth value

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,109 +1,109 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container login-container px-4 px-md-0">
-        <div class="row register-height justify-content-center">
-            <div class="login-title col-12 col-md-5  ">Registrati per inserire il tuo appartamento <i
-                    class="d-none d-md-inline fa-solid fa-arrow-right"></i><i
-                    class="d-inline d-md-none fa-solid fa-arrow-down"></i></div>
-            <div class="login-form-container d-flex align-items-center py-5 py-md-0 col-12 col-md-7 ">
-                <form class="login-form" method="POST" action="{{ route('register') }}" id="registrationForm">
-                    @csrf
-                    <div class="form-floating mb-3">
-                        {{-- input e label --}}
-                        <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
-                            placeholder="Nome" name="name" value="{{ old('name') }}" autocomplete="name" autofocus>
-                        <label for="name">Nome</label>
+<div class="container login-container px-4 px-md-0">
+    <div class="row register-height justify-content-center">
+        <div class="login-title col-12 col-md-5  ">Registrati per inserire il tuo appartamento <i
+                class="d-none d-md-inline fa-solid fa-arrow-right"></i><i
+                class="d-inline d-md-none fa-solid fa-arrow-down"></i></div>
+        <div class="login-form-container d-flex align-items-center py-5 py-md-0 col-12 col-md-7 ">
+            <form class="login-form" method="POST" action="{{ route('register') }}" id="registrationForm">
+                @csrf
+                <div class="form-floating mb-3">
+                    {{-- input e label --}}
+                    <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
+                        placeholder="Nome" name="name" value="{{ old('name') }}" autocomplete="name" autofocus>
+                    <label for="name">Nome</label>
 
-                        {{-- controlli ed errori --}}
-                        <div class="invalid-feedback" id="nameError">
-                            Il nome deve contenere almeno 2 caratteri e solo lettere.
-                        </div>
+                    {{-- controlli ed errori --}}
+                    <div class="invalid-feedback" id="nameError">
+                        Il nome deve contenere almeno 2 caratteri e solo lettere.
                     </div>
+                </div>
 
-                    <div class="form-floating mb-3">
-                        {{-- input and label --}}
-                        <input type="text" class="form-control @error('surname') is-invalid @enderror" id="surname"
-                            placeholder="Cognome" name="surname" autocomplete="surname" value="{{ old('surname') }}"
-                            autofocus>
-                        <label for="surname">Cognome</label>
-                        {{-- errori --}}
-                        <div class="invalid-feedback" id="surnameError">
-                            Il cognome deve contenere almeno 2 caratteri e solo lettere.
-                        </div>
+                <div class="form-floating mb-3">
+                    {{-- input and label --}}
+                    <input type="text" class="form-control @error('surname') is-invalid @enderror" id="surname"
+                        placeholder="Cognome" name="surname" autocomplete="surname" value="{{ old('surname') }}"
+                        autofocus>
+                    <label for="surname">Cognome</label>
+                    {{-- errori --}}
+                    <div class="invalid-feedback" id="surnameError">
+                        Il cognome deve contenere almeno 2 caratteri e solo lettere.
                     </div>
+                </div>
 
-                    <div class="form-floating mb-3">
-                        {{-- input and label --}}
-                        <input type="date" class="form-control @error('date_of_birth') is-invalid @enderror"
-                            id="date_of_birth" name="date_of_birth" autocomplete="date_of_birth"
-                            value="{{ old('date_of_birth', Carbon\Carbon::now()->subYears(18)->toDateString()) }}"
-                            autocomplete="date_of_birth" autofocus>
+                <div class="form-floating mb-3">
+                    {{-- input and label --}}
+                    <input type="date" class="form-control @error('date_of_birth') is-invalid @enderror"
+                        id="date_of_birth" name="date_of_birth" autocomplete="date_of_birth"
+                        max="{{ Carbon\Carbon::now()->subYears(18)->toDateString() }}" autocomplete="date_of_birth"
+                        autofocus>
 
-                        <label for="date_of_birth">Data di nascita</label>
+                    <label for="date_of_birth">Data di nascita</label>
 
-                        {{-- errori --}}
-                        <div class="invalid-feedback" id="dobError">
-                            Devi avere almeno 18 anni.
-                        </div>
+                    {{-- errori --}}
+                    <div class="invalid-feedback" id="dobError">
+                        Devi avere almeno 18 anni.
                     </div>
+                </div>
 
-                    <div class="form-floating mb-3">
-                        {{-- input and label --}}
-                        <input type="email" class="form-control @error('email') is-invalid @enderror" id="email"
-                            placeholder="Email" name="email" autocomplete="email" value="{{ old('email') }}" autofocus
-                            required>
-                        <label for="email">Indirizzo email</label>
+                <div class="form-floating mb-3">
+                    {{-- input and label --}}
+                    <input type="email" class="form-control @error('email') is-invalid @enderror" id="email"
+                        placeholder="Email" name="email" autocomplete="email" value="{{ old('email') }}" autofocus
+                        required>
+                    <label for="email">Indirizzo email</label>
 
-                        {{-- errori --}}
-                        <div class="invalid-feedback" id="emailError">Inserisci un indirizzo email valido.</div>
-                        <div class="invalid-feedback" id="emailErrorMissingAt" style="display:none;">Manca il
-                            simbolo "@".</div>
-                        <div class="invalid-feedback" id="emailErrorMissingDomain" style="display:none;">
-                            L'indirizzo email deve includere un dominio (es. gmail.com).
-                        </div>
-                        <div class="invalid-feedback" id="emailErrorInvalidCharacters" style="display:none;">
-                            L'indirizzo email contiene caratteri non validi.
-                        </div>
+                    {{-- errori --}}
+                    <div class="invalid-feedback" id="emailError">Inserisci un indirizzo email valido.</div>
+                    <div class="invalid-feedback" id="emailErrorMissingAt" style="display:none;">Manca il
+                        simbolo "@".</div>
+                    <div class="invalid-feedback" id="emailErrorMissingDomain" style="display:none;">
+                        L'indirizzo email deve includere un dominio (es. gmail.com).
                     </div>
-                    <div class="form-floating mb-3">
-                        {{-- input and label --}}
-                        <input type="password" class="form-control @error('password') is-invalid @enderror" id="password"
-                            name="password" autocomplete="new-password" placeholder="Password" autofocus required>
-                        <label for="password">Password</label>
-
-                        {{-- errori --}}
-                        <div class="invalid-feedback" id="passwordError">
-                            La password deve contenere almeno 8 caratteri.
-                        </div>
+                    <div class="invalid-feedback" id="emailErrorInvalidCharacters" style="display:none;">
+                        L'indirizzo email contiene caratteri non validi.
                     </div>
+                </div>
+                <div class="form-floating mb-3">
+                    {{-- input and label --}}
+                    <input type="password" class="form-control @error('password') is-invalid @enderror" id="password"
+                        name="password" autocomplete="new-password" placeholder="Password" autofocus required>
+                    <label for="password">Password</label>
 
-                    <div class="form-floating mb-3">
-                        {{-- input and label --}}
-                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password"
-                            name="password_confirmation" autocomplete="new-password" autofocus required>
-                        <label for="floatingPassword">Conferma Password *</label>
-
-                        {{-- errori --}}
-                        <div class="invalid-feedback" id="passwordConfirmError">
-                            Le password non coincidono.
-                        </div>
+                    {{-- errori --}}
+                    <div class="invalid-feedback" id="passwordError">
+                        La password deve contenere almeno 8 caratteri.
                     </div>
+                </div>
 
+                <div class="form-floating mb-3">
+                    {{-- input and label --}}
+                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password"
+                        name="password_confirmation" autocomplete="new-password" autofocus required>
+                    <label for="floatingPassword">Conferma Password *</label>
 
-                    <div>
-                        <button type="submit" class="btn login-btn">{{ __('Registrati') }}</button>
+                    {{-- errori --}}
+                    <div class="invalid-feedback" id="passwordConfirmError">
+                        Le password non coincidono.
                     </div>
-
-                </form>
-            </div>
+                </div>
 
 
+                <div>
+                    <button type="submit" class="btn login-btn">{{ __('Registrati') }}</button>
+                </div>
+
+            </form>
         </div>
-    </div>
 
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
+
+    </div>
+</div>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
             // Aggiungi la validazione in tempo reale ai campi input
             document.getElementById('name').addEventListener('input', validateName);
             document.getElementById('surname').addEventListener('input', validateSurname);
@@ -283,18 +283,18 @@
             }
             return age;
         }
-    </script>
+</script>
 
-    <style>
-        .is-valid {
-            border-color: #28a745;
-            background-color: #d4edda;
-            color: #28a745;
-        }
+<style>
+    .is-valid {
+        border-color: #28a745;
+        background-color: #d4edda;
+        color: #28a745;
+    }
 
-        .is-invalid {
-            border-color: #dc3545;
-            background-color: #f8d7da;
-        }
-    </style>
+    .is-invalid {
+        border-color: #dc3545;
+        background-color: #f8d7da;
+    }
+</style>
 @endsection
